21e04f451b28617620307b504200816ec7a962fb
Esha Pandey <esha.pandey@pdc01.lisnepal.com.np>
2024-06-24 11:06:20 +0545

    changes --

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
   disks = [
-    { "id": 1, "dev": "sdb", "lvm": 0, "sizeGB": 16, "dir": "/data1" }
+    { "id": 1, "dev": "sdb", "lvm": 0, "sizeGB": tonumber(var.disksize), "dir": "/data1" }
   ]
 
   disk_format_args = join(" ", [
-    for disk in local.disks: "${disk.dev},${disk.lvm},${disk.sizeGB},${disk.dir}"
+    for disk in local.disks : "${disk.dev},${disk.lvm},${disk.sizeGB},${disk.dir}"
   ])
 }
 
@@ -25,31 +25,57 @@ data "vsphere_compute_cluster" "cluster" {
 data "vsphere_network" "network" {
   name          = var.network
   datacenter_id = data.vsphere_datacenter.datacenter.id
-
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "my-ucentos-vm"
+  for_each         = var.vms
+  name             = each.value.name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 2
-  memory           = 4096
-  guest_id         = "CentOS64Guest"
-  
+  num_cpus         = tonumber(var.vminfo[each.key].cpu)
+  memory           = tonumber(var.vminfo[each.key].memory)
+  guest_id         = "centos64Guest"
+  scsi_type        = "lsilogic-sas"
+
   network_interface {
-    network_id = data.vsphere_network.network.id
-    
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
   }
 
   disk {
     label = "disk0"
-    size  = 40
+    size  = tonumber(var.disksize)
   }
 
- cdrom {
+  cdrom {
     datastore_id = data.vsphere_datastore.datastore.id
-    path         = "/vmfs/volumes/Database-Server-B-Datastore/ISO/CentOS-7-x86_64-DVD-2009.iso"
+    path         = "/vmfs/volumes/${var.datastore}/ISO/CentOS-7-x86_64-DVD-2009.iso"
   }
 
-   
+  dynamic "disk" {
+    for_each = local.disks
+    content {
+      label           = "disk${disk.value.id}"
+      size            = disk.value.sizeGB
+      unit_number     = disk.value.id
+      eagerly_scrub   = false
+      thin_provisioned = true
+    }
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkfs.ext4 /dev/sdb",
+      "sudo mkdir -p /data1",
+      "sudo mount /dev/sdb /data1",
+      "echo '/dev/sdb /data1 ext4 defaults 0 0' | sudo tee -a /etc/fstab"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = var.jumphost_user
+      password = var.jumphost_password
+      host     = each.value.vm_ip
+    }
+  }
+}
