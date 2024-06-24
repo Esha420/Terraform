160a2ea3136f4d6f27d887aabe6551654c5b219f
Esha Pandey <esha.pandey@pdc01.lisnepal.com.np>
2024-06-24 11:44:47 +0545

    changes --

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 data "vsphere_datacenter" "dc" {
   name = var.datacenter
 }
@@ -19,14 +18,13 @@ data "vsphere_network" "network" {
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "test-vm"
+  name             = var.vms["rocky_test_1"]["name"]
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 1
-  memory   = 1024
-  guest_id = "centos64Guest"
-
+  num_cpus         = 2
+  memory           = 4096  # in MB (4 GB)
+  guest_id         = "centos64Guest"  # Adjust guest_id based on your operating system
+  
   network_interface {
     network_id   = data.vsphere_network.network.id
     adapter_type = "vmxnet3"
@@ -34,13 +32,24 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label            = "disk0"
-    size             = 10
-    eagerly_scrub    = false
+    size             = var.disksize  # Convert to GB
     thin_provisioned = true
   }
 
   cdrom {
     datastore_id = data.vsphere_datastore.datastore.id
-    path         = "/vmfs/volumes/${var.datastore}/ISO/CentOS-7-x86_64-DVD-2009.iso"
+    path         = "/vmfs/volumes/${var.datastore}/ISO/CentOS-7-x86_64-DVD-2009.iso"  # Adjust path as per your ISO
   }
+
+  # Customize further properties as needed (e.g., CPU limits, custom scripts, etc.)
+
+}
+
+# Outputs for useful information
+output "vm_name" {
+  value = vsphere_virtual_machine.vm.name
+}
+
+output "vm_ip_address" {
+  value = var.vms["rocky_test_1"]["vm_ip"]
 }
