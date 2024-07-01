3735c3d4eebaa7f0109d071587e803463e90fb64
Esha420 <pandey.isha420@gmail.com>
2024-07-01 13:18:41 +0545

    Python file for GUI

---
+++ main1.tf
@@ -0,0 +1,97 @@
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  count            = length(var.vms)
+  name             = values(var.vms)[count.index].name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus             = values(var.vms)[count.index].cpu
+  memory               = values(var.vms)[count.index].memory
+  guest_id             = values(var.vms)[count.index].guest_id
+  cpu_hot_add_enabled  = true
+  memory_hot_add_enabled = true
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = values(var.vms)[count.index].disksize
+    eagerly_scrub    = false
+    thin_provisioned = false
+  }
+  
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = "ISO/CentOS-7-x86_64-DVD-2009.iso"
+  }
+
+  extra_config = {
+    "guestinfo.userdata" = <<EOF
+#cloud-config
+users:
+  - default
+  - name: ${values(var.vms)[count.index].username}
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: users, admin
+    home: /home/${values(var.vms)[count.index].username}
+    shell: /bin/bash
+    lock_passwd: false
+    passwd: ${values(var.vms)[count.index].password}
+
+network:
+  version: 2
+  ethernets:
+    eth0:
+      addresses:
+        - ${values(var.vms)[count.index].vm_ip}/${values(var.vms)[count.index].ipv4_netmask}
+      gateway4: ${values(var.vms)[count.index].ipv4_gateway}
+      nameservers:
+        addresses: ${jsonencode(var.dns_server_list)}
+        search: ${jsonencode(var.dns_suffix_list)}
+EOF
+  }
+
+  wait_for_guest_net_timeout = 120
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo -e '${values(var.vms)[count.index].password}\n${values(var.vms)[count.index].password}' | passwd ${values(var.vms)[count.index].username}"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "12345"  # Replace with the actual root password or another initial password
+      host     = values(var.vms)[count.index].vm_ip
+    }
+  }
+}
+
+output "vm_ips" {
+  value = { for k, v in var.vms : k => v.vm_ip }
+}
+
+output "vm_names" {
+  value = { for k, v in var.vms : k => v.name }
+}
---
+++ terraform1.tfvars
@@ -0,0 +1,43 @@
+vCenter_user       = "Administrator@vcsa.local"
+vCenter_password   = "Tu$P@ssw0rdVr@"
+vCenter_server     = "172.25.204.15"
+
+
+jumphost_ip           = "172.25.204.50"
+jumphost_subnet       = "24"
+jumphost_gateway      = "172.25.204.1"
+
+jumphost_user         = "tuadmin"
+jumphost_password     = "TU@123"
+
+dns_server_list       = [ "10.11.10.69" ]
+dns_suffix_list       = [ "tudc.com" ]
+
+disksize               = "20"
+
+vms = {
+   "VM_1"= {
+    name       = "VM-01"
+    vm_ip      = "172.25.204.49"
+    cpu        = 2
+    memory     = 1024
+    disksize   = 40
+    guest_id   = "centos7_64Guest"
+    ipv4_netmask = 24
+    ipv4_gateway = "172.25.204.1"
+    username    = "kube-spray"
+    password    = "12345"
+  }
+   "VM_2"= {
+    name                = "VM-02"
+    vm_ip               = "172.25.204.50"
+    cpu        = 2
+    memory     = 1024
+    disksize   = 40
+    guest_id   = "centos7_64Guest"
+    ipv4_netmask = 24
+    ipv4_gateway = "172.25.204.1"
+    username    = "kube-spray"
+    password    = "12345"
+  }
+}
